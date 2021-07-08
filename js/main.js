317857b3a841572e29e4fad4fdf110c62dd6a2c6
Shahjada87 <71275176+Shahjada87@users.noreply.github.com>
2021-07-08 08:36:25 +0530

    Added css to make it responsive

--- js/main.js
+++ js/main.js
@@ -18,9 +18,19 @@ $(window).on("load", function() {
 
 
 $(document).ready(function() {
+// nav toggle
+$(".hamburger-btn").click(function(){
+  $(".header .nav").slideToggle();
+})
+$(".header .nav a").click(function(){
+  if($(window).width() < 768){
+    $(".header .nav").slideToggle();
+  }
+})
+
 //fixed header
 $(window).scroll(function(){
-  if ($(this).scrollTop() > 700){
+  if ($(this).scrollTop() > 100){
     $(".header").addClass("fixed");
   }else{
     $(".header").removeClass("fixed");
